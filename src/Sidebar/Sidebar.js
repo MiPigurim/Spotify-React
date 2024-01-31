541c138db53e9e773acf15f911eae5a2c9adb532
MiPigurim <michelepigurim26@gmail.com>
2024-01-31 17:03:22 -0300

    feat: sidebar navegation

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,5 +1,34 @@
 import React from "react";
 import "./Sidebar.css";
+import logoSpotify from "../assets/icons/logo-spotify.png"
 
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+    <nav className="sidebar__navigation">
+      <div className="sidebar__logo">
+        <img src={logoSpotify} alt="Logo Spotify" />
+      </div>
+      <ul>
+        <li>
+          <a href="">
+            <span></span>
+            <span>Início</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span></span>
+            <span>Busca</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+
+   
+    </div>
+  )
+}
 
 export default Sidebar;
