0d55287a4d2c8015bb4ab3565779c04f6e1f4d80
MiPigurim <michelepigurim26@gmail.com>
2024-02-01 17:00:56 -0300

    feat: import main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header/Header';
-import Sidebar from './Sidebar/Sidebar'
+import Sidebar from './Sidebar/Sidebar';
+import Main from './Main/Main';
 
 function App() {
   return (
     <div>
       <Sidebar></Sidebar>
       <Header></Header>
+      <Main></Main>
    </div>
   );
 }
